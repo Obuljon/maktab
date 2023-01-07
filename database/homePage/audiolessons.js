ec15bad134e527d65685dca4dc748ab03c56d68f
Obuljon <rahimovabul2@gmail.com>
2023-01-07 20:40:36 +0500

    data basni zattim

---
+++ database/homePage/audiolessons.js
@@ -0,0 +1,65 @@
+export default [
+    {
+        img: "audiolessons/1.jpg",
+        fan: "Ispan-tili",
+        kun: "210",
+        h4: "Ispan-tili uchun yangi Kurs",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, placeat, ipsa. Modi sed quibusdam vel autem fugit, eaque, iste. Excepturi fugit dignissimos suscipit dolor perferendis debitis magni sed, quia ab.",
+        narxi: "",
+        _id: "audioIspanTili",
+        title:"Ispan-tili",
+    },
+    {
+        img: "audiolessons/2.jpg",
+        fan: "Ingiliz-tili",
+        kun: "210",
+        h4: "Ingiliz-tili uchun yangi Kurs",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, placeat, ipsa. Modi sed quibusdam vel autem fugit, eaque, iste. Excepturi fugit dignissimos suscipit dolor perferendis debitis magni sed, quia ab.",
+        narxi: "$165.00",
+        _id: "audioIngilizTili",
+        title:"Ingiliz-tili",
+    },
+    {
+        img: "audiolessons/3.jpg",
+        fan: "Fransuz-tili",
+        kun: "210",
+        h4: "Fransuz-tili uchun yangi Kurs",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, placeat, ipsa. Modi sed quibusdam vel autem fugit, eaque, iste. Excepturi fugit dignissimos suscipit dolor perferendis debitis magni sed, quia ab.",
+        narxi: "$165.00",
+        _id: "audioFransuzTili",
+        title:"Fransuz-tili",
+      
+    },
+    {
+        img: "audiolessons/4.jpg",
+        fan: "Nems-tili",
+        kun: "102",
+        h4: "Nems-tili uchun yangi Kurs",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, placeat, ipsa. Modi sed quibusdam vel autem fugit, eaque, iste. Excepturi fugit dignissimos suscipit dolor perferendis debitis magni sed, quia ab.",
+        narxi: "$165.00",
+        _id: "audioNemsTili",
+        title:"Nems-tili",
+       
+    },
+    {
+        img: "audiolessons/5.jpg",
+        fan: "Kores-tili",
+        kun: "75",
+        h4: "kores-tili uchun yangi Kurs",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, placeat, ipsa. Modi sed quibusdam vel autem fugit, eaque, iste. Excepturi fugit dignissimos suscipit dolor perferendis debitis magni sed, quia ab.",
+        narxi: "$165.00",
+        _id: "audioKoresTili",
+        title:"Kores-tili",
+       
+    },
+    {
+        img: "audiolessons/6.jpg",
+        fan: "Rus - tili",
+        kun: "210",
+        h4: "rus-tili uchun yangi Kurs",
+        p: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, placeat, ipsa. Modi sed quibusdam vel autem fugit, eaque, iste. Excepturi fugit dignissimos suscipit dolor perferendis debitis magni sed, quia ab.",
+        narxi: "$165.00",
+        _id: "audioRusTili",
+        title:"Rus - tili"
+    },
+]
