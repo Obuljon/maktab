2f7def6c21b367575e4ba4ff9a3a879c52c24545
Obuljon <rahimovabul2@gmail.com>
2023-01-10 02:50:16 +0500

    features ni zattim

--- controllers/homePage/home.controller.js
+++ controllers/homePage/home.controller.js
@@ -6,35 +6,30 @@ import teacherComtent from "../../database/homePage/teacher.comtent.js";
 import testimonial from "../../database/homePage/testimonial.js";
 import course from "../../database/kurs/course.js";
 import fromblog from "../../database/blog/fromblog.js";
+
 class About{
     aboutHome(req, res){
         res.render("main/about", {features, slider, teacherComtent, counter, testimonial, service, course, fromblog })
     }
-    proFessionalCourses(req, res){
-        let course = professionalcourses;
-        res.render("main/coursearchive", { course, title: "proFessional kurslar" })
+
+    ourfeatures(req, res){
+        const id = req.params._id;
+        if(features.find(item => item._id == id)){
+            const course = features.find(item => item._id == id).detail
+            res.render("main/coursearchive", { course, title: `${features.find(item => item._id == id).nameh4}` })
+        }else{
+            features.forEach(item => {
+                const course = item.detail
+                if(item.detail.find(elment => elment._id == id)){
+                    const fan = item.detail.find(elment => elment._id == id)
+                    return res.render("main/detail", { fan, course })
+                }
+            })
+        }
     }
-    expertteachers(req, res){
-        let course = expertTeachers;
-        res.render("main/coursearchive", { course, title: "Malakali o'qtuvchilar" })
-    }
-    onlineLearning(req, res){
-        let course = onlinelearning;
-        res.render("main/coursearchive", { course, title: "Online talimlar" })
-    }
-    audioLessons(req, res){
-        let course = audiolessons;
-        res.render("main/coursearchive", { course, title: "Audio darslar" }) 
-    }
-    scientificfilms(req, res){
-        let course = scientificfilms;
-        res.render("main/coursearchive", { course, title: "Ilmiy filmlar" }) 
-    }
-    professionalCertificate(req, res){
-        const course = professionalcertificate;
-        res.render("main/coursearchive", { course, title: "Professional Certificate" })
-    }
+    
 }
 export default new About()
+
 
 
--- routers/homePage/homerouter.js
+++ routers/homePage/homerouter.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import HomePage from "../../controllers/homePage/home.controller.js"
 const router = new Router();
 
-router.get("/", HomePage.aboutHome )
+router.get("/", HomePage.aboutHome );
+router.get("/:_id", HomePage.ourfeatures);
+
 
 export default router
 
