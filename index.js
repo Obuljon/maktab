4d84cd6eafccae25a8bacf5b2b5d5914ac62c53e
azizbekoffical <azizbekrozmetov734@gmail.com>
2023-01-12 14:49:37 +0500

    Kurs

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import express from 'express';
 import { connect } from 'mongoose';
 import session from 'express-session';
 import layout from 'express-ejs-layouts';
+
 import HomeRouter from './routers/main.router/main.router.js'
 import GalleryRouter from './routers/gallery_router/gallery.router.js';
 import Homepage from "./routers/homePage/homerouter.js"
 import ContactRouter from './routers/contact_router/contact_router.js';
 import Contact_sms_Router from './routers/contactsms_router/contact_sms_router.js';
 import DeleteRouter from './routers/delete_router/delete.router.js';
+
+
+
 connect('mongodb://localhost/maktab', {useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 
@@ -33,4 +37,6 @@ app.use(GalleryRouter);
 app.use(ContactRouter);
 app.use(Contact_sms_Router);
 app.use(DeleteRouter);
+
+
 app.listen(2066, () => console.log('server is runnning Port:8080'));
--- routers/Course/course.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import Course from "../../controllers/course/couurse.controller";
-import IsAuth from '../controllers/isasuth.controller.js';
-
-const router = new Router();
-
-Router.get('/coursearchive', IsAuth, Course.CoursearchivePage);
-
-export default router;
