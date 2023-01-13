d00c6b8fb7c334c0042e79897c54b89d28449deb
Obuljon <rahimovabul2@gmail.com>
2023-01-13 18:18:31 +0500

    ..

--- routers/detail_router/detail.router.js
+++ routers/detail_router/detail.router.js
@@ -1,11 +1,6 @@
 import { Router } from "express";
 import Detail from '../../controllers/detail/detail_main.js';
 const router = new Router();
-<<<<<<< HEAD
 router.get('/detail/:id', Detail.detailpageId);
-=======
-// ____ ?
-router.get('/detail', Detail.detailpageId);
->>>>>>> c34544342b9ab8ae1c01f49726a16ab0d3098251
 
 export default router;
